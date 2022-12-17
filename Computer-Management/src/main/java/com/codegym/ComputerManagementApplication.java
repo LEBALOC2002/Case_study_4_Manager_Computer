91fb25f198d7a14e1137a6db865439253e1a6ed8
LOC SHADOW <lochuedvfb@gmail.com>
2022-12-17 11:17:22 +0700

    Cu

---
+++ Computer-Management/src/main/java/com/codegym/ComputerManagementApplication.java
@@ -0,0 +1,13 @@
+package com.codegym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComputerManagementApplication.class, args);
+    }
+
+}
